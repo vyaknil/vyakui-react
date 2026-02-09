25058aed436572fff8849fd87b0c71e1a08e0f99
vyaknil <vyaknil@gmail.com>
2026-02-09 19:36:33 +0400

    real?

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 import "./global.css";
-import { color as _color } from './types/color'
-import { font as _font, getFont as _getFont } from './types/font'
-import { getRem as _getRem, mapRem as _mapRem } from './types/number'
-import { size as _size } from './types/size'
 
+export { color } from './types/color';
+export { font, getFont } from './types/font';
+export { size } from './types/size';
+export { getRem, mapRem } from './types/number';
+
+export { VFlex, VLink, VButton } from "./components";
+export { Styled, useVRegistry, VRegistry, createCSS, generateHash } from "./styled";
 
 export type { VFlexProps } from "./components";
-export { VFlex, VLink, VButton } from "./components";
-
-
 export type { VStyle } from "./styled";
-export { Styled, useVRegistry, VRegistry, createCSS, generateHash } from "./styled";
 export type { Colors, Color, Font, Size, Numbers } from "./types";
-
-export const color = _color;
-export const font = _font;
-export const size = _size;
-export const getFont = _getFont;
-export const getRem = _getRem;
-export const mapRem = _mapRem;
